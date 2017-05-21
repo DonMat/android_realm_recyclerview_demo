20eec06277ba133818f2ef14e808b2ed24435216
Mateusz Utkała <donmat@outlook.com>
2017-05-21 22:52:39 +0200

    add realm recycler view to main activity

--- app/src/main/java/pl/utkala/realmdemo/MainActivity.java
+++ app/src/main/java/pl/utkala/realmdemo/MainActivity.java
@@ -3,11 +3,28 @@ package pl.utkala.realmdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import co.moonmonkeylabs.realmrecyclerview.RealmRecyclerView;
+import io.realm.Realm;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RealmRecyclerView recyclerView;
+    private Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        realm = Realm.getDefaultInstance();
+
+        recyclerView = (RealmRecyclerView) findViewById(R.id.realm_recycler_view);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if(realm != null)
+            realm.close();
+        super.onDestroy();
     }
 }
