574c48bf65d9df2cd435a6de2182deea865b1200
Mateusz Utkała <donmat@outlook.com>
2017-05-21 23:41:11 +0200

    add person class

---
+++ app/src/main/java/pl/utkala/realmdemo/models/Person.java
@@ -0,0 +1,45 @@
+package pl.utkala.realmdemo.models;
+
+import java.util.Locale;
+import java.util.UUID;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Person extends RealmObject {
+    public static final String NAME = "name";
+    public static final String SURNAME = "surname";
+    public static final String AGE = "age";
+
+    @PrimaryKey
+    private String id = UUID.randomUUID().toString();
+    private String name;
+    private String surname;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.getDefault(), "%s %s, %d", name, surname, age);
+    }
+}
